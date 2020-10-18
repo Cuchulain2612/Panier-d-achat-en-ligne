67617e55f098a005ed7a20435084380d358d321e
Cuchulain2612 <71312603+Cuchulain2612@users.noreply.github.com>
2020-10-18 14:48:12 +0200

    Update projet.js

--- TR04/js/projet.js
+++ TR04/js/projet.js
@@ -1,12 +1,24 @@
 "use strict";
-let listeArticle =[];
-let sommeTotal =0;
+let listeArticle =[];  // liste des article            
+let sommeTotal =0;    // future somme totale à payer
 
-
+/**
+*Fonction ajoutant un article à la liste des articles
+*
+*@param {string} string contenant le nom de l'article à ajouter
+*@return ajoute à la fin de la liste le nouvel articele choisi
+*/
 function additionEtArray(stringArticle) {
     listeArticle.push(stringArticle);
 
 }
+
+/**
+*Fonction supprimant de la liste des articles un article au choix grâce à une boucle for permettant de le retrouver dans cette liste
+*
+*@param {string} string contenant le nom de l'article à retirer
+*@return retire de la liste l'articele choisi
+*/
 function soustraireEtArray(stringArticle) {
     for(let compteur = 1; compteur < listeArticle.length; compteur +=1){
         if (listeArticle[compteur] === stringArticle) {
@@ -15,6 +27,14 @@ function soustraireEtArray(stringArticle) {
         }
     }
 }
+
+/**
+*Fonction calculant la somme totale à payer en utilisant une boucle pour vérifier tous les éléments de la liste. 
+*On utilise un switch pour vérifier de quel article il sagit
+*
+*
+*@return {} affiche en console la somme total à payer ainsi que le prix moyen d'un article
+*/
 function resultat() {
     for (let compteur =0; compteur < listeArticle.length; compteur +=1) {
         switch (listeArticle[compteur]) {
